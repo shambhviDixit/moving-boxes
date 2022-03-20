d80938678d70cea04bae823f8687b00ed83702c1
shambhviDixit <99592580+shambhviDixit@users.noreply.github.com>
2022-03-20 16:52:37 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+
+function setup() 
+{
+  createCanvas(400, 400);
+box=new  Box(100,200,20,20,2);
+box1=new Box(200,100,20,20,2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+function draw() 
+{
+  background(220);
+ box.show();
+ box.move();
+ box1.show();
+ box1.move();
+
+}
